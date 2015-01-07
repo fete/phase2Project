5ddf40aa46f392d8cd566272a8bb6ba9eccaa813
fete <d1swaay@gmail.com>
2015-01-07 10:23:27 -0500

    added event migration

--- db/migrate/20150107100139_create_events.rb
+++ db/migrate/20150107100139_create_events.rb
@@ -1,4 +1,13 @@
 class CreateEvents < ActiveRecord::Migration
   def change
+  	create_table :tasks do |t|
+ 	  t.string     :title
+ 	  t.text       :description
+ 	  t.string     :url
+ 	  t.references :user
+
+ 	  t.timestamps
+ 	end
+
   end
 end
