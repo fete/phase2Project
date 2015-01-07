dcfd75b6bbcd2b76b36ebad929969768fb9f7e02
fete <d1swaay@gmail.com>
2015-01-07 14:29:24 -0500

    added single event route

--- app/controllers/event.rb
+++ app/controllers/event.rb
@@ -2,8 +2,13 @@ get '/' do
   redirect '/events/all'
 end
 
+#single event
+get '/event/:id' do |id| 
+  @task = Event.find(id) #@task is used only when you want erb to have access to the var
+  erb :'event/single', locals: {event: @event}
+end
 
-
+#all events
 get '/events/all' do
   redirect '/login' unless current_user
   @events = current_user.events 
