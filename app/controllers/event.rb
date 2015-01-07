041bf69dce6887c3054a60c89fe3a3803505e2e6
fete <d1swaay@gmail.com>
2015-01-07 13:59:31 -0500

    added create new event route

--- app/controllers/event.rb
+++ app/controllers/event.rb
@@ -2,8 +2,23 @@ get '/' do
   redirect '/events/all'
 end
 
+
+
 get '/events/all' do
   redirect '/login' unless current_user
   @events = current_user.events 
   erb :'event/all'
 end
+
+
+#create
+get '/events/new' do 
+  erb :'event/new' 
+end
+
+
+post '/events' do 
+  params[:event][:user_id] = current_user.id 
+  redirect ("/event/#{event.id}")
+end
+
