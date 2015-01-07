7222c1a0f7229b0c5ea7f0d13e202b8747b53dbd
fete <d1swaay@gmail.com>
2015-01-07 15:52:17 -0500

    made relationship one to many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password 
   validates :username, presence: true
-  #admin default value
-  
   has_many :user_events
-  has_many :events, through: :user_events
 end
