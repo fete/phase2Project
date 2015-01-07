ee3fd10903cf87877ec8857f9f3723fcb64fbba5
fete <d1swaay@gmail.com>
2015-01-07 11:09:39 -0500

    edited table name

--- db/migrate/20150107100454_create_user_events.rb
+++ db/migrate/20150107100454_create_user_events.rb
@@ -1,6 +1,6 @@
 class CreateUserEvents < ActiveRecord::Migration
   def change
-  	create_table :users do |t|
+  	create_table :user_events do |t|
   	  t.references :user
   	  t.references :event 
 
